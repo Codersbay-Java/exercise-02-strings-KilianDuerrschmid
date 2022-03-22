9965f45bad947f2005e7441767ff6830c12f829f
KilianDuerrschmid <k.duerrschmid@gmx.at>
2022-03-22 09:55:30 +0100

    Fertige Aufgabe

--- src/main/java/application/PlayWithStrings.java
+++ src/main/java/application/PlayWithStrings.java
@@ -1,8 +1,21 @@
 package application;
 
+import java.util.Arrays;
+
 public class PlayWithStrings {
 	public static void main(String[] args) {
-		System.out.println("Hello String World");
+		String firstname = "Kilian";
+		String lastname = "Duerrschmid";
+
+		printInitials(firstname, lastname);
+
+		String value1 = "parliament";
+		String value2 = "partial men";
+
+		System.out.println(areAnagrams(value1, value2));
+
+		String value = "evacanIseebeesinaca ve";
+		System.out.println(isPalindrome(value));
 	}
 
 	/**
@@ -13,8 +26,8 @@ public class PlayWithStrings {
 	 * @param firstname
 	 * @param lastname
 	 */
-	public static void printIntitials(String firstname, String lastname) {
-
+	public static void printInitials(String firstname, String lastname) {
+		System.out.println(firstname.substring(0, 1) + lastname.substring(0, 1));
 	}
 
 	/**
@@ -31,7 +44,51 @@ public class PlayWithStrings {
 	 * @return true if the values are anagrams, false otherwise.
 	 */
 	public static boolean areAnagrams(String value1, String value2) {
+
+		value1 = removeJunk(value1);
+		value2 = removeJunk(value2);
+
+		if (value1.length() == value2.length()) {
+
+			char[] charArr1 = value1.toCharArray();
+			char[] charArr2 = value2.toCharArray();
+
+			Arrays.sort(charArr1);
+			Arrays.sort(charArr2);
+
+			boolean anagram = Arrays.equals(charArr1, charArr2);
+
+			if (anagram) {
+				return true;
+			}
+
+		}
 		return false;
+
+// add ascii values and see if they are equal
+
+//		int asc1 = 0;
+//		int asc2 = 0;
+//
+//		value1 = removeJunk(value1);
+//		value2 = removeJunk(value2);
+//
+//		if (value1.length() == value2.length()) {
+//
+//			for (int i = 0; i < value1.length(); i++) {
+//				asc1 += value1.codePointAt(i);
+//			}
+//
+//			for (int i = 0; i < value2.length(); i++) {
+//				asc2 += value2.codePointAt(i);
+//			}
+//
+//			if (asc1 == asc2) {
+//				return true;
+//			}
+//		}
+//		return false;
+
 	}
 
 	/**
@@ -49,6 +106,23 @@ public class PlayWithStrings {
 	 * @return true if it is a palindrome, false otherwise.
 	 */
 	public static boolean isPalindrome(String value) {
-		return false;
+		value = removeJunk(value);
+
+		String reverted = new StringBuilder(value).reverse().toString();
+
+		return value.equalsIgnoreCase(reverted);
+	}
+
+	public static String removeJunk(String input) {
+		StringBuilder builder = new StringBuilder();
+		char[] chars = input.toCharArray();
+
+		for (char c : chars) {
+			if (Character.isLetter(c)) {
+				builder.append(c);
+			}
+		}
+
+		return builder.toString();
 	}
 }
